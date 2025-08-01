55e7d68b5439425306acdd70aacc1796967fd01a
Pomaransky <61422562+Pomaransky@users.noreply.github.com>
2025-08-01 20:31:44 +0200

    AWS-6 implement edit form dialog (#6)
    * Enhance Profile Page with Edit Functionality
    
    - Introduced EditProfileDialog component for user profile editing.
    - Added "Edit Profile" button to the Profile page for triggering the dialog.
    - Implemented form handling for first name and last name updates.
    - Integrated user data update functionality in UserService with success/error notifications.
    - Improved confirm dialog styling for better responsiveness.
    
    * Update README.md with local development instructions for Angular and Strapi app

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# angular-with-strapi
-angular-with-strapi
+# Angular with strapi app
+
+#### To develop locally:
+
+1. Run **npm run dev** in **/strapi**
+2. Run **npm run start** in **/frontend**
--- frontend/src/app/core/services/user-service.ts
+++ frontend/src/app/core/services/user-service.ts
@@ -1,15 +1,18 @@
 import { inject, Injectable } from '@angular/core';
 import { ApiService } from './api-service';
 import { HttpClient } from '@angular/common/http';
-import { catchError, finalize, Observable, of, tap } from 'rxjs';
+import { catchError, finalize, Observable, of, tap, throwError } from 'rxjs';
 import { User } from '../models/auth';
 import { UserStore } from '../store/user.store';
+import { MessageService } from 'primeng/api';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService extends ApiService {
   private userStore = inject(UserStore);
+  private messageService = inject(MessageService);
+
   constructor(http: HttpClient) {
     super(http);
   }
@@ -25,6 +28,29 @@ export class UserService extends ApiService {
       }),
       finalize(() => {
         this.userStore.setLoading(false);
+      }),
+    );
+  }
+
+  editUserData(
+    user: Pick<User, 'id' | 'firstName' | 'lastName'>,
+  ): Observable<User> {
+    return this.put<User>(`users/${user.id}`, user).pipe(
+      tap((user) => {
+        this.userStore.setUser(user);
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Success',
+          detail: 'User data updated successfully',
+        });
+      }),
+      catchError((error) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: error.error.error.message,
+        });
+        return throwError(() => new Error(error.error.error.message));
       }),
     );
   }
