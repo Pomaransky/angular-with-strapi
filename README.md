7a1643301406e9d1602259d81fabbef92f0d645c
Pomaransky <61422562+Pomaransky@users.noreply.github.com>
2025-08-21 12:44:17 +0200

    AWS-10-Improve-register-user-data-model-update (#10)
    - Update README.md 
    - Add strapi lifecycle hooks (create user-data after register)

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 #### To develop locally:
 
-1. Run **npm run dev** in **/strapi**
-2. Run **npm run start** in **/frontend**
+1. Run **npm install** in the **/strapi** directory
+2. Create a **.env** file based on **.env.example** in the **/strapi** directory
+3. Run **npm run dev** in the **/strapi** directory
+4. Create a Strapi admin account (local)
+5. Create an admin user
+
+6. Run **npm install** in the **/frontend** directory
+7. Run **npm run start** in the **/frontend** directory
+8. Log in to the created admin user or register a new user
--- strapi/src/index.ts
+++ strapi/src/index.ts
@@ -1,4 +1,49 @@
-// import type { Core } from '@strapi/strapi';
+import type { Core } from '@strapi/strapi';
+
+async function checkIfUserDataExists(userId: string) {
+  const existingUserProfile = await strapi
+    .documents("api::user-data.user-data")
+    .findMany({
+      filters: {
+        user: {
+          id: {
+            $eq: userId,
+          },
+        },
+      },
+    });
+
+  return existingUserProfile;
+}
+
+async function createUserData(userId: string) {
+  const userProfile = await checkIfUserDataExists(userId);
+
+  if (userProfile.length > 0) {
+    return;
+  }
+
+  const newUserData = await strapi.documents("api::user-data.user-data").create({
+    data: {
+      user: userId,
+    },
+  });
+
+  await strapi.documents("api::user-data.user-data").publish({
+    documentId: newUserData.documentId,
+  });
+}
+
+async function deleteUserData(userId: string) {
+  const userProfile = await checkIfUserDataExists(userId);
+
+  if (userProfile.length > 0) {
+    await strapi.documents("api::user-data.user-data").delete({
+      documentId: userProfile[0].documentId,
+    });
+  }
+}
+
 
 export default {
   /**
@@ -16,5 +61,18 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
+  bootstrap({ strapi }: { strapi: Core.Strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: ['plugin::users-permissions.user'],
+      async afterCreate (event) {
+        const { result } = event;
+        await createUserData(result.id);
+      },
+      async beforeDelete (event: any) {
+        const { params } = event;
+        const idToDelete = params?.where?.id;
+        await deleteUserData(idToDelete);
+      },
+    });
+  },
 };
