f1a98909e7ac56e69dd4b8511ddc67c2525cdde4
Arek <pomaransky97@gmail.com>
2026-02-03 11:14:41 +0100

    Add reusable table component, Implement new table component in users table

--- frontend/src/app/core/components/index.ts
+++ frontend/src/app/core/components/index.ts
@@ -2,3 +2,4 @@ export * from './confirm-dialog/confirm-dialog';
 export * from './spinner/spinner';
 export * from './data-row/data-row';
 export * from './input-field/input-field';
+export * from './table/table';
---
+++ frontend/src/app/core/models/table-column-type.enum.ts
@@ -0,0 +1,7 @@
+export enum TableColumnType {
+  STRING = 'string',
+  NUMBER = 'number',
+  BOOLEAN = 'boolean',
+  DATE = 'date',
+  STATUS_TAG = 'status-tag',
+}
